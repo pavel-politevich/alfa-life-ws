5315781c6e0b845a88d12b9a85efc3ba908a833e
Pavel <52207587+pavel-politevich@users.noreply.github.com>
2023-07-18 10:14:00 +0300

    Update AlfapayRequestDto.java

--- src/main/java/by/lifetech/alfalife/dto/AlfapayRequestDto.java
+++ src/main/java/by/lifetech/alfalife/dto/AlfapayRequestDto.java
@@ -5,12 +5,12 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 
 
